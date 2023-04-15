74ef1ae60985cda2b762127472f46bd7954593d4
Edison Muñoz <edison.munoz1901@alumnos.ubiobio.cl>
2023-04-14 22:19:03 -0400

    v0.9

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-<<<<<<< HEAD
 import { Box, Grid, Paper, IconButton, Button, Grow } from '@mui/material';
-=======
-import { Box, Grid, Paper, IconButton, Button } from '@mui/material';
->>>>>>> origin/nelson2
 import axios from 'axios';
 import like from './assets/like.png'
 import dislike from './assets/dislike.png'
